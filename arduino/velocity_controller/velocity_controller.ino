393ab898628fd176d28837c3fae27421fcd4b99d
Rom Parnichkun <rom.parnichkun@gmail.com>
2021-09-27 21:04:08 +0900

    can compute velocity now

--- arduino/velocity_controller/velocity_controller.ino
+++ arduino/velocity_controller/velocity_controller.ino
@@ -12,16 +12,24 @@ const int MOTOR_RIGHT_SPEED = 7;
 const int MOTOR_RIGHT_A = 8;
 const int MOTOR_RIGHT_B = 9;
 
+const double PID[3] = {0.01, 0.0, 0.0};
+
 volatile long int count_left = 0;
 volatile long int count_right = 0;
 
-float desired_vel_left = 0.0;
-float desired_vel_right = 0.0;
+double desired_vel_left = 0.0;
+double desired_vel_right = 0.0;
+double actual_vel_left = 0.0;
+double actual_vel_right = 0.0;
 
 void update_left_a();
 void update_left_b();
 void update_right_a();
 void update_right_b();
+
+double control(double desired, double actual, double pid[3]);
+double convert_to_radians(int count);
+void compute_vel();
 
 void setup() {
   Serial.begin(250000);
@@ -40,11 +48,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Count Left: ");
-  Serial.print(count_left);
-  Serial.print("\tCount Right: ");
-  Serial.println(count_right);
+  compute_vel();
+  Serial.print("Angle Left: ");
+  Serial.print(convert_to_radians(count_left));
+  Serial.print("\tAngle Right: ");
+  Serial.print(convert_to_radians(count_right));
+  Serial.print("\tVel Left: ");
+  Serial.print(actual_vel_left, 6);
+  Serial.print("\tVel Right: ");
+  Serial.println(actual_vel_right,6);
+  
   delay(10);
+
 }
 
 void update_left_a() {
@@ -133,4 +148,29 @@ void update_right_b() {
       count_right++;
     }
   }
+}
+
+double control(double desired, double actual, double pid[3]){
+  
+  
+}
+
+double convert_to_radians(int count){
+  return (2*PI*(double)count)/double(COUNT_REV);
+}
+
+void compute_vel(){
+  static double previous_angle_left = convert_to_radians(count_left);
+  static double previous_angle_right = convert_to_radians(count_right);
+  static unsigned long int previous_time = micros();
+  double current_angle_left = convert_to_radians(count_left);
+  double current_angle_right = convert_to_radians(count_right);
+  double current_time = micros();
+
+  actual_vel_left = (current_angle_left-previous_angle_left)/((double)(current_time-previous_time)/1000000.0);
+  actual_vel_right = (current_angle_right-previous_angle_right)/((double)(current_time-previous_time)/1000000.0);
+
+  previous_angle_left = current_angle_left;
+  previous_angle_right = current_angle_right;
+  previous_time = current_time;
 }
